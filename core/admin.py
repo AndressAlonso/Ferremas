18844b07ed03bd41f2c48043d3dd2c6df3132e4a
unknown <Laboratorio@MPAPCA03LC0111.duoc.acad>
2024-05-29 16:44:41 -0400

    creacion de base de datos, tabla Producto, tipo producto y marca

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Marca)
+admin.site.register(Tipo_producto)
+admin.site.register(Producto)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    id_marca = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20, null=True)
+    def __str__(self):
+        return self.nombre
+    
+class Tipo_producto(models.Model):
+    id_tipo_producto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20, null=True)
+    def __str__(self):
+        return self.nombre
+    
+class Producto (models.Model):
+    id = models.AutoField(primary_key=True)
+    id_marca = models.ForeignKey(to=Marca, on_delete=models.CASCADE)
+    id_tipo_producto = models.ForeignKey(to=Tipo_producto,on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=50)
+    stock = models.IntegerField()
+    imagen = models.CharField(max_length=255)
+    precio = models.IntegerField()
+    
+    def __str__(self):
+        return self.descripcion 
