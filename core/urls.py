2f4ee57ff69dca2dbe2e593d938f7d4a42f6aea7
Andres Alvarado <alvaradoandres130@gmail.com>
2024-06-09 15:56:30 -0400

    Login Django Hecho y Cambios en el modal, login y Home

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
+
 from .views import *
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', home, name="home"),
     path('form', form, name="form"),
     path('carrito', carrito, name="carrito"),
-    path('login', login, name="login"),
+    path('registro', registro, name="registro"),
     path('detalle', detalle, name="detalle"),
+    path('logout', logout, name="logout"),
+    path('login', LoginView.as_view(template_name="login.html"), name="login"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.views import logout_then_login
 
 def home(request):
     productos = Producto.objects.all()
@@ -11,9 +12,12 @@ def form(request):
 def carrito(request):
     return render(request, "Carrito.html")
 
-def login(request):
-    return render(request, "Login.html")
+def registro(request):
+    return render(request, "registro.html")
 
 def detalle(request):
     return render(request, "detalle.html")
+
+def logout(request):
+    return logout_then_login(request, 'login')
 
