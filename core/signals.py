30f2649890c843fe9b54a1638b7e10a9df7064ce
Andres Alvarado <alvaradoandres130@gmail.com>
2025-05-23 16:35:28 -0400

    Login y registro cambiado por ferremas, carga de productos con imagenes

--- core/signals.py
+++ core/signals.py
@@ -11,23 +11,23 @@ def crear_perfil_usuario(sender, instance, created, **kwargs):
     if created:
         PerfilUsuario.objects.create(user=instance)
 
-@receiver(pre_save, sender=Pedido)
-def notificar_cambio_estado(sender, instance, **kwargs):
-    if not instance.pk:
-        return
+# @receiver(pre_save, sender=Pedido)
+# def notificar_cambio_estado(sender, instance, **kwargs):
+#     if not instance.pk:
+#         return
 
-    try:
-        pedido_anterior = Pedido.objects.get(pk=instance.pk)
-    except Pedido.DoesNotExist:
-        return
+#     try:
+#         pedido_anterior = Pedido.objects.get(pk=instance.pk)
+#     except Pedido.DoesNotExist:
+#         return
 
-    if pedido_anterior.estado != instance.estado:
-        perfil = instance.cliente.perfilusuario
-        telefono = perfil.telefono
-        nombre = instance.cliente.first_name
-        estado = instance.get_estado_display()
+#     if pedido_anterior.estado != instance.estado:
+#         perfil = instance.cliente.perfilusuario
+#         telefono = perfil.telefono
+#         nombre = instance.cliente.first_name
+#         estado = instance.get_estado_display()
 
-        if telefono:
-            numero = ''.join(filter(str.isdigit, telefono))  
-            enviar_mensaje_template(numero, nombre, instance.id, estado)
+#         if telefono:
+#             numero = ''.join(filter(str.isdigit, telefono))  
+#             enviar_mensaje_template(numero, nombre, instance.id, estado)
 
